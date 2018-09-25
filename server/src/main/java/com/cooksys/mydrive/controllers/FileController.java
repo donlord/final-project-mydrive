02aa4d235bdb2ce15c627f8f52d8b80db2d68f95
unknown <alex@stageworthy.net>
2018-09-25 14:37:01 -0500

    Added functionality to FolderController

--- server/src/main/java/com/cooksys/mydrive/controllers/FileController.java
+++ server/src/main/java/com/cooksys/mydrive/controllers/FileController.java
@@ -6,5 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="/files")
 public class FileController {
+    //get {path}
 
+    //post {path}
+
+    //patch {path}/trash
+
+    //delete {path}
 }
--- server/src/main/java/com/cooksys/mydrive/controllers/FolderController.java
+++ server/src/main/java/com/cooksys/mydrive/controllers/FolderController.java
@@ -1,10 +1,49 @@
 package com.cooksys.mydrive.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/folders")
 public class FolderController {
-	
+
+    //private FolderServices folderServices;
+
+    /* GET /folders
+    @GetMapping
+    public List<Object> returnTopLevel () {
+        return folderServices.get();
+    }
+    */
+
+	/* GET /folders/{path}
+	@GetMapping("/{path}")
+	public Folder returnSingleFolder (@PathVariable(path="folderPath") String folderPath) {
+	    return folderServices.getFolder(folderPath);
+	}
+	*/
+
+	/* POST /folders/{path}
+    @PostMapping("/{path}")
+    public Folder createNewFolder (@PathVariable(path="folderPath") String folderPath) {
+        return folderServices.createNewFolder(folderPath);
+    }
+	*/
+
+    /*
+    @PatchMapping("/{path}/trash")
+    public Folder trashFolder (@PathVariable(path="folderPath") String folderPath) {
+        return folderServices.trashSelectedFolder(folderPath);
+    }
+    */
+
+    /*
+    @DeleteMapping("/{path}")
+    public Folder deleteSelectedFolderFromTrash (@PathVariable(path="folderPath") String folderPath) {
+        return folderServices.deleteSelectedFolderFromTrash(folderPath);
+    }
+    */
 }
