b442f05745fba4f6351cdc389ccd08314a5fec6a
unknown <alex@stageworthy.net>
2018-09-26 11:44:09 -0500

    Added Upload Table Component

---
+++ client/src/components/Home/UploadTable.js
@@ -0,0 +1,32 @@
+const UploadTableStyle = styled.div`
+ position: absolute;
+ top: 20%;
+ left: 65%;
+ width: 500px;
+ font-size: 16pt;
+ background-color: white;
+ border: 1px solid black;
+`
+
+class UploadTable extends React.Component {
+  render () {
+    return (
+      <form
+        method='POST'
+        enctype='multipart/form-data'
+        action='/files/example'
+      >
+        <UploadTableStyle>
+          <table>
+            <tr>
+              <td>File to upload:</td><td><input type='file' name='file' /></td>
+            </tr>
+            <tr><td /><td><input type='submit' value='Upload' /></td></tr>
+          </table>
+        </UploadTableStyle>        
+      </form>
+    )
+  }
+}
+
+export default UploadTable
